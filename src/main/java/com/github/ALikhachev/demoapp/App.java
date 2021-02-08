4bb4f2ed631eb8b68c72970db8d5f48808b59687
Alexander Likhachev <Alexander.Likhachev@jetbrains.com>
2021-02-08 03:14:59 +0300

    Initial commit

---
+++ src/main/java/com/github/ALikhachev/demoapp/App.java
@@ -0,0 +1,7 @@
+package com.github.ALikhachev.demoapp;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.printf("%d x %d = %d\n", 2, 2, Utils.square(2));
+    }
+}
---
+++ src/main/java/com/github/ALikhachev/demoapp/Utils.java
@@ -0,0 +1,7 @@
+package com.github.ALikhachev.demoapp;
+
+public class Utils {
+    static int square(int number) {
+        return number * 2;
+    }
+}
